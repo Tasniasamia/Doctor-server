16237caa58f8b3230fb91407f203c364c9325301
tasniaarham <tasniaarhamarham@gmail.com>
2023-05-14 14:05:06 +0600

    yesss

--- index.js
+++ index.js
@@ -19,13 +19,21 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+  maxPoolSize:10
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   await client.connect((err)=>{
+if(err){
+  console.error(err);
+  return;
+}
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
 
